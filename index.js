2e80982a9f7553318350a0e8d964bce03674c582
Alok Menghrajani <441307+alokmenghrajani@users.noreply.github.com>
2022-06-01 22:27:48 +0200

    Some polish

--- index.js
+++ index.js
@@ -11,6 +11,37 @@
 const {Puzzle: Puzzle, Difficulty: Difficulty} = require('./puzzle.js');
 const HtmlUI = require('./html-ui.js');
 
+function positionBoards() {
+  const r = global.container.getBoundingClientRect();
+  const board = global.board;
+  const goal = global.goal;
+
+  if (r.width > r.height) {
+    // board size must be <= r.height and 66% of r.width
+    const s = (Math.min(r.height, 0.66 * (r.width - 10)))|0;
+    board.style.width = s;
+    board.style.height = s;
+    goal.style.width = s/3;
+    goal.style.height = s/3;
+    goal.style.left = s + 10;
+    goal.style.top = 2*s/3;
+  } else {
+    // board size must be <= r.width and 66% of r.height
+    const s = (Math.min(r.width, 0.66 * (r.height - 10)))|0;
+    board.style.width = s;
+    board.style.height = s;
+    goal.style.width = s/3;
+    goal.style.height = s/3;
+    goal.style.left = 2*s/3;
+    goal.style.top = s + 10;
+  }
+  alok = board;
+
+}
+
+window.onload = () => positionBoards();
+window.onresize = () => positionBoards();
+
 function loadLevel(level) {
   let puzzle;
   switch (level) {
@@ -18,32 +49,46 @@ function loadLevel(level) {
       puzzle = new Puzzle([["A", "B", "A", "B"], ["B", "A", "B", "A"], ["A", "B", "A", "B"], ["B", "A", "B", "A"]], Difficulty.Easy);
       puzzle.shuffle(100);
       new HtmlUI(puzzle, board, goal);
+      global.footer.innerText = "Current level: Easy"
       break;
     case "medium":
       puzzle = new Puzzle([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], Difficulty.Easy);
       puzzle.shuffle(100);
       new HtmlUI(puzzle, board, goal);
+      global.footer.innerText = "Current level: Medium"
       break;
     case "hard":
       puzzle = new Puzzle([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], Difficulty.Hard);
       puzzle.shuffle(100);
       new HtmlUI(puzzle, board, goal);
+      global.footer.innerText = "Current level: Hard"
       break;
   }
 }
 
 // default easy
-global.levelEasy.checked = true;
 loadLevel("easy");
 
-global.levelEasy.onchange = () => loadLevel("easy");
-global.levelMedium.onchange = () => loadLevel("medium");
-global.levelHard.onchange = () => loadLevel("hard");
-
-global.showInstructions.onclick = () => {
-  document.body.classList.add("instr");
+global.showInstructions.onclick = (e) => {
+  document.body.classList.add("hide");
+  global.instr.classList.add("enabled");
 }
 
-global.hideInstructions.onclick = () => {
-  document.body.classList.remove("instr");
+global.instr.onclick = () => {
+  document.body.classList.remove("hide");
+  global.instr.classList.remove("enabled");
+}
+
+global.showLevels.onclick = () => {
+  document.body.classList.add("hide");
+  global.levels.classList.add("enabled");
+}
+
+global.easy.onclick = () => loadLevel("easy");
+global.medium.onclick = () => loadLevel("medium");
+global.hard.onclick = () => loadLevel("hard");
+
+global.levels.onclick = () => {
+  document.body.classList.remove("hide");
+  global.levels.classList.remove("enabled");
 }
